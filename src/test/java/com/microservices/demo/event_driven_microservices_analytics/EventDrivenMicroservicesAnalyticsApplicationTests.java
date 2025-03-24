3d0334c82a936748bdb3e16f25d6d9019b03b21b
Sahithi Gaddam <gaddam.sahi@northeastern.edu>
2025-03-24 12:11:26 -0400

    Initial commit

---
+++ src/test/java/com/microservices/demo/event_driven_microservices_analytics/EventDrivenMicroservicesAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.event_driven_microservices_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenMicroservicesAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
